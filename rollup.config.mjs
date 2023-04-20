5f2a1127b8bbd61f6bb3eaacded63bb7183fce6a
Daniil Leonov <yshmgrt@gmail.com>
2023-04-20 13:18:22 +0300

    Quickfix build config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,7 @@ import typescript from '@rollup/plugin-typescript';
 export default [{
   input: 'src/index.ts',
   output: {
-    dir: 'build/cylindrical-panorama-viewer.js',
+    file: 'build/cylindrical-panorama-viewer.js'
   },
   plugins: [
     typescript({tsconfig: 'tsconfig.json'}),
